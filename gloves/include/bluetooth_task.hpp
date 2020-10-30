48d2ed045df0068c608871104acd491871316b47
MHovenkamp <m.hovenkamp@outlook.com>
2020-10-30 12:59:23 +0100

    added some bluetotoh features, untested

--- gloves/include/bluetooth_task.hpp
+++ gloves/include/bluetooth_task.hpp
@@ -5,13 +5,22 @@
 
 # include "support.hpp"
 # include "input_object.hpp"
+#include <ArduinoBLE.h>
+
+enum class BLUETOOTH_STATES{
+    INITIALIZE,
+    WAIT_FOR_CONNECTION,
+    UPDATE_CHARACTERISTIC,
+    CONNECTED,
+    PREPARE_DATA
+};
 
 class Bluetooth_task{
 private:
+    BLE_service glove_peripheral("180F");
     BLE_device BLE_device_phone;
-    char[11] advertised_service = 'HonestHands';
+    const char* advertised_service = "HonestHands";
     char[4] device_name = 'HAND';
-    char* data;
     uint8_t data_length = 255;
     bool active_connection;
     InputObject mode_switch
@@ -20,5 +29,6 @@ public:
         mode_switch(mode_switch)
     {}
     void main();
+    // source = https://ladvien.com/arduino-nano-33-bluetooth-low-energy-setup/
 };
 # endif
--- gloves/src/bluetooth_task.cpp
+++ gloves/src/bluetooth_task.cpp
@@ -1,1 +1,69 @@
 // bluetooth task
+
+# include "../../gloves/include/bluetooth_task.hpp"
+
+void bluetooth_task::main(){
+    BLUETOOTH_STATES state = BLUETOOTH_STATES::INITIALIZE
+    switch(state){
+        case(BLUETOOTH_STATES::INITIALIZE):{
+            BLEUnsignedCharCharacteristic glove_peripheral_char_1("2A19", BLERead | BLENotify);  // standard 16-bit characteristic UUID
+            BLEUnsignedCharCharacteristic glove_peripheral_char_2("2A19", BLERead | BLENotify);  // standard 16-bit characteristic UUID
+            BLEUnsignedCharCharacteristic glove_peripheral_char_3("2A19", BLERead | BLENotify);  // standard 16-bit characteristic UUID
+            BLEUnsignedCharCharacteristic glove_peripheral_char_4("2A19", BLERead | BLENotify);  // standard 16-bit characteristic UUID
+            BLEUnsignedCharCharacteristic glove_peripheral_char_5("2A19", BLERead | BLENotify);  // standard 16-bit characteristic UUID
+            BLEUnsignedCharCharacteristic glove_peripheral_char_6("2A19", BLERead | BLENotify);  // standard 16-bit characteristic UUID
+            BLEUnsignedCharCharacteristic glove_peripheral_char_7("2A19", BLERead | BLENotify);  // standard 16-bit characteristic UUID
+            BLEUnsignedCharCharacteristic glove_peripheral_char_8("2A19", BLERead | BLENotify);  // standard 16-bit characteristic UUID
+            BLEUnsignedCharCharacteristic glove_peripheral_char_9("2A19", BLERead | BLENotify);  // standard 16-bit characteristic UUID
+            BLEUnsignedCharCharacteristic glove_peripheral_char_10("2A19", BLERead | BLENotify);  // standard 16-bit characteristic UUID
+            
+            if (!BLE.begin()) {
+                Serial.println("starting BLE failed!");
+                while (1);
+            }
+
+            BLE.setLocalName(advertised_service);
+            BLE.setAdvertisedService(glove_peripheral);
+            glove_peripheral.addCharacteristic(glove_peripheral_char_1);
+            glove_peripheral.addCharacteristic(glove_peripheral_char_2);
+            glove_peripheral.addCharacteristic(glove_peripheral_char_3);
+            glove_peripheral.addCharacteristic(glove_peripheral_char_4);
+            glove_peripheral.addCharacteristic(glove_peripheral_char_5);
+            glove_peripheral.addCharacteristic(glove_peripheral_char_6);
+            glove_peripheral.addCharacteristic(glove_peripheral_char_7);
+            glove_peripheral.addCharacteristic(glove_peripheral_char_8);
+            glove_peripheral.addCharacteristic(glove_peripheral_char_9);
+            glove_peripheral.addCharacteristic(glove_peripheral_char_10);
+            BLE.addService(glove_peripheral);
+
+
+            BLE.advertise();
+            state = BLUETOOTH_STATES::WAIT_FOR_CONNECTION;
+            break;
+        }
+        case(BLUETOOTH_STATES::WAIT_FOR_CONNECTION){
+            BLE_device_phone = BLE.central();
+            if(BLE_device_phone){
+                state = BLUETOOTH_STATES::CONNECTED;
+                break;
+            }
+        }
+        case(BLUETOOTH_STATES::CONNECTED){
+            while(BLE_device_phone.connected()){
+                // check if there is new valid data
+                uint8_t[10] old_data;
+                uint8_t[10] data;
+                // data = read from bluetooth_queue
+                if( data != old_data ){
+                    // put individual numbers into richt characteristic
+                }
+            }
+            state = BLUETOOTH_STATES::WAIT_FOR_CONNECTION;
+            break;
+        }
+
+    }
+
+
+
+}
