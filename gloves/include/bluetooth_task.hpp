482abb4919196913f7c0b7f564d97b5c8491e0ce
MHovenkamp <m.hovenkamp@outlook.com>
2020-10-30 10:27:28 +0100

    lost some code in FreeRtos pushes, returned code

--- gloves/include/bluetooth_task.hpp
+++ gloves/include/bluetooth_task.hpp
@@ -1,1 +1,24 @@
 // bluetooth task
+
+# ifndef _BLUETOOTH_TASK_HPP
+# define _BLUETOOTH_TASK_HPP
+
+# include "support.hpp"
+# include "input_object.hpp"
+
+class Bluetooth_task{
+private:
+    BLE_device BLE_device_phone;
+    char[11] advertised_service = 'HonestHands';
+    char[4] device_name = 'HAND';
+    char* data;
+    uint8_t data_length = 255;
+    bool active_connection;
+    InputObject mode_switch
+public:
+    Bluetooth_task( InputObject mode_switch):
+        mode_switch(mode_switch)
+    {}
+    void main();
+};
+# endif
