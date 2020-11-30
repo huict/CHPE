0088667bbb01d21e68d8c27c3f6c700571e8ce55
Anwar <anwar.sakkali.kk@gmail.com>
2020-11-30 17:12:04 +0100

    merge fix

--- android/src/com/mygdx/honestmirror/application/common/videohandler/VideoThread.java
+++ android/src/com/mygdx/honestmirror/application/common/videohandler/VideoThread.java
@@ -1,11 +1,30 @@
 package com.mygdx.honestmirror.application.common.videohandler;
 
 import android.graphics.Bitmap;
+import android.media.MediaMetadataRetriever;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
 import com.mygdx.honestmirror.application.nnanalysis.poseestimation.nn.PoseNet.Person;
 import com.mygdx.honestmirror.application.nnanalysis.poseestimation.nn.PoseNet.PoseNetHandler;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
+
+class ThreadWithQueue extends Thread {
+    private int threadAmount;
+    private MediaMetadataRetriever mediaMetadataRetriever;
+    private BlockingQueue frameQueue;
+
+    @RequiresApi(api = Build.VERSION_CODES.O_MR1)
+    public ThreadWithQueue(int threadAmount, MediaMetadataRetriever mediaMetadataRetriever) {
+        this.threadAmount = threadAmount;
+        this.mediaMetadataRetriever = mediaMetadataRetriever;
+
+        Bitmap bitmap = this.mediaMetadataRetriever.getScaledFrameAtTime(1, 0, 257, 257);
+    }
+}
 
 class Thread1 extends Thread {
     List<Person> persons = new ArrayList<>();
