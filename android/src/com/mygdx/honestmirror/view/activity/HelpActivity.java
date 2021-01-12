dd2ff1a94b7263dc0b2f3142fc1456cb55b8f5c7
Anwar <anwar.sakkali.kk@gmail.com>
2021-01-12 15:11:33 +0100

    help page updated and home page color change

--- android/src/com/mygdx/honestmirror/view/activity/HelpActivity.java
+++ android/src/com/mygdx/honestmirror/view/activity/HelpActivity.java
@@ -2,13 +2,33 @@ package com.mygdx.honestmirror.view.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.mygdx.honestmirror.R;
+import com.mygdx.honestmirror.view.ui.a_Home;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HelpActivity extends AppCompatActivity {
+    Button previous;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        previous = findViewById(R.id.previous);
+
+        previous.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            Intent intent = new Intent(HelpActivity.this, a_Home.class);
+            startActivity(intent);
+            }
+        });
     }
+
+
+
+
 }
